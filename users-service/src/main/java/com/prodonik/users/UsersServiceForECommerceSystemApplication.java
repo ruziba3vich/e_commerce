98ef1c4cab415a084d1a18177d706034bfa5f126
prodonik <rruzibayevich@gmail.com>
2024-07-01 22:11:12 +0500

    e-commerce system

---
+++ users-service/src/main/java/com/prodonik/users/UsersServiceForECommerceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.prodonik.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceForECommerceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersServiceForECommerceSystemApplication.class, args);
+	}
+
+}
---
+++ users-service/src/test/java/com/prodonik/users/UsersServiceForECommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodonik.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceForECommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
